fc226a6b202b754ba8c386cb1a3d8bdb5f7420f6
Curtis Maloney <curtis@tinbrain.net>
2016-02-23 15:47:56 +1100

    add static module

---
+++ ablaze/static.py
@@ -0,0 +1,12 @@
+'''
+Add a route handler for static content.
+
+[static]
+url = /static/
+path = static/
+
+'''
+
+async def setup(app):
+    config = app['config']['static']
+    app.router.add_static(config['url'], config['path'])
