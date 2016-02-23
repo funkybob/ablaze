a9f381b42d5017d2897fddf3e953354e89fa4d66
Curtis Maloney <curtis@tinbrain.net>
2016-02-23 16:26:26 +1100

    Disable defaults section for now.  Add logging

--- ablaze/base.py
+++ ablaze/base.py
@@ -4,12 +4,15 @@ from importlib import import_module
 
 from aiohttp import web
 
+import logging
+log = logging.getLogger(__name__)
+
 
 async def build_app(loop, config_path, defaults=None):
     config_path = os.path.abspath(config_path)
     if defaults is None:
         defaults = {}
-    defaults.setdefault('CONFIG_DIR', os.path.dirname(config_path))
+    # defaults.setdefault('CONFIG_DIR', os.path.dirname(config_path))
     config = configparser.ConfigParser(defaults=defaults)
     config.read(config_path)
 
@@ -18,6 +21,7 @@ async def build_app(loop, config_path, defaults=None):
 
     modules = config['ablaze']
     for key, module in modules.items():
+        log.info('Loading modul: %s -> %s', module, key)
         cfg = config[key]
         pkg = import_module(module, 'ablaze')
         await pkg.setup(app, cfg)
