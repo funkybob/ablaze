a8d3900d69b8b1787c43b804bff9e3c491f0b03a
Curtis Maloney <curtis@tinbrain.net>
2016-02-23 12:40:35 +1100

    run_app doesn't take a loop argument

--- ablaze/__init__.py
+++ ablaze/__init__.py
@@ -6,4 +6,4 @@ from .base import build_app  # NOQA
 def launch(config_path):
     loop = asyncio.get_event_loop()
     app = loop.run_until_complete(build_app(loop, config_path))
-    run_app(app, host='127.0.0.1', port=8080, loop=loop)
+    run_app(app, host='127.0.0.1', port=8080)
